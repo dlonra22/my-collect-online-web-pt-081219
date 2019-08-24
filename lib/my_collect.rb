ab08bed0e3daa7766e6a99adc7995b0055759306
arnie b <arnold.bwaila@gmail.com>
2019-08-24 11:31:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 def my_collect(array)
-   if 
+   if block_given?
+     i = 0 
+     collection = []
+     while i < array.length 
+      collect << yield array[i]
+      i+=1 
+      end
+      collection
+    end
