df27c397ce310279e73c2b8d35ef279564e91000
arnie b <arnold.bwaila@gmail.com>
2019-08-24 11:32:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,9 @@ def my_collect(array)
      i = 0 
      collection = []
      while i < array.length 
-      collect << yield array[i]
+      collection << yield array[i]
       i+=1 
       end
       collection
     end
+  end
